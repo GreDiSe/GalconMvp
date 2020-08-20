0b08eced6d95684c3c86666125a0dcb00c5e56e3
GreDiSe <kykysya1999@gmail.com>
2020-08-20 18:47:40 +0300

    Updates

---
+++ precache-manifest.8c6b079c5f7675a35998bc01b6645644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a90e71dbf16c9cc635f62b71ed14327e",
+    "url": "/galcon-mvp/index.html"
+  },
+  {
+    "revision": "9173746d261823f5d415",
+    "url": "/galcon-mvp/static/css/main.a8c1c994.chunk.css"
+  },
+  {
+    "revision": "4adf27bef0206a55c07a",
+    "url": "/galcon-mvp/static/js/2.59dd3844.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/galcon-mvp/static/js/2.59dd3844.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9173746d261823f5d415",
+    "url": "/galcon-mvp/static/js/main.eaad778c.chunk.js"
+  },
+  {
+    "revision": "9815bf1f47ff3e6f30c6",
+    "url": "/galcon-mvp/static/js/runtime-main.6910708d.js"
+  }
+]);
